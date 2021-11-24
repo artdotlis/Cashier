72a6b4b453ab026470b858dbb6ca2eb8d956a961
Artur Lissin <arturonrails@protonmail.com>
2021-11-24 20:26:15 +0100

    docs: Document tax_calculator.py

--- src/cashier/register/tax_calculator.py
+++ src/cashier/register/tax_calculator.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Module used for calculating taxes."""
 import decimal
 from decimal import Decimal
 from typing import final
@@ -8,7 +9,22 @@ from src.cashier.register.container import PurchasedItem
 
 @final
 class TaxCalculator:
+    """
+    Calculates sales taxes for a purchased item.
+
+    Notes
+    -----
+        ``TaxCalculator`` is a ``@final`` class and thus should not be used as a superclass.
+    """
     def __init__(self, import_taxes: float, normal_taxes: float, /) -> None:
+        """
+        Parameters
+        ----------
+        import_taxes : `float`
+            The sales taxes for imported items.
+        normal_taxes : `float`
+            The basic sales taxes.
+        """
         super().__init__()
         # precision for: one decimal place, two decimal places
         self.__precision: tuple[Decimal, Decimal] = (Decimal("0.0"), Decimal("0.00"))
@@ -19,7 +35,32 @@ class TaxCalculator:
         )
 
     def _calc_tax(self, price: Decimal, tax: Decimal, cnt: int, /) -> Decimal:
-        # round(20 * total) / 20,  but more accurate
+        """
+        Calculates sales taxes.
+
+        This method returns the sales tax for ``cnt`` items
+        with the same ``price``. The sales tax value is rounder and
+        formatted based on the rules defined in ``TaxCalculator``.
+
+        Parameters
+        ----------
+        price : `decimal.Decimal`
+            The price of the purchased item.
+        tax : `decimal.Decimal`
+            The sales tax, which will be used in the tax-calculation.
+        cnt : int
+            The amount of the purchased item.
+
+        Returns
+        -------
+        `decimal.Decimal`
+            Calculated and formatted sales taxes.
+
+        Notes
+        -----
+            The taxes are rounded similar to the``round(20 * total) / 20``
+            approach and only print two decimal places.
+        """
         decimal.getcontext().rounding = decimal.ROUND_HALF_UP
         taxes: Decimal = (
                 price * tax * self.__multiplier * cnt
@@ -28,6 +69,19 @@ class TaxCalculator:
         return taxes.quantize(self.__precision[1])
 
     def tax(self, p_item: PurchasedItem, /) -> Decimal:
+        """
+        Calculates sales taxes for a purchased item.
+
+        Parameters
+        ----------
+        p_item: `src.cashier.register.container.PurchasedItem`
+            The purchased item.
+
+        Returns
+        -------
+        `decimal.Decimal`
+            Calculated and formatted sales taxes for the purchased item ``p_item``.
+        """
         abs_price: Decimal = abs(p_item.price)
         imported_tax: Decimal = self.__taxes[0] if p_item.imported else self.__taxes[2]
         if p_item.taxed:
@@ -39,9 +93,25 @@ class TaxCalculator:
         return self.__taxes[2].quantize(self.__precision[1])
 
     def new_import_tax(self, tax: float, /) -> None:
+        """
+        Sets a new value for the import sales taxes.
+
+        Parameters
+        ----------
+        tax : `float`
+            The new value for the import sales taxes.
+        """
         self.__taxes = (Decimal(str(tax)), self.__taxes[1], self.__taxes[2])
 
     def new_normal_tax(self, tax: float, /) -> None:
+        """
+        Sets a new value for the basic sales taxes.
+
+        Parameters
+        ----------
+        tax : `float`
+            The new value for the basic sales taxes.
+        """
         self.__taxes = (self.__taxes[0], Decimal(str(tax)), self.__taxes[2])
 
     def __str__(self) -> str:
