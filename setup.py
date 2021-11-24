2e0ac0288641c38282f715a3b6f83af948d6a566
Artur Lissin <arturonrails@protonmail.com>
2021-11-24 18:08:57 +0100

    feat: Add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# https://packaging.python.org/tutorials/packaging-projects/
+from pathlib import Path
+import setuptools  # type: ignore
+
+from src.cashier.version import VERSION
+
+
+with Path("README.md").open("r") as fh_read_me:
+    READ_ME = fh_read_me.read()
+
+setuptools.setup(
+    name="Cashier",
+    version=VERSION,
+    author="Artur Lissin",
+    author_email="arturOnRails@protonmail.com",
+    maintainer="Artur Lissin",
+    maintainer_email="arturOnRails@protonmail.com",
+    description="Library for calculating sales taxes.",
+    long_description=READ_ME,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arturOnRails/Cashier",
+    platforms=["Linux"],
+    license='MIT License',
+    packages=setuptools.find_packages(),
+    python_requires="==3.10",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License"
+    ],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'cashier=cashier.main:main'
+        ]
+    }
+)
