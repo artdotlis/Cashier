7a01daba7660b1214b2dce6b25c66ee607863e47
Artur Lissin <arturonrails@protonmail.com>
2021-11-28 12:49:39 +0100

    chore: Improve sphinx code style with black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,17 +5,15 @@ from pathlib import Path
 from src.cashier.version import get_version
 
 
-sys.path.insert(
-    0, str(Path(".").absolute().parent.parent.joinpath('src'))
-)
+sys.path.insert(0, str(Path(".").absolute().parent.parent.joinpath("src")))
 
 
 # -- Project information -------------------------------------------------------
 
-project = 'Cashier'
-copyright = '2021, Artur Lissin'
-author = 'Artur Lissin'
-release = 'v0.1.0'
+project = "Cashier"
+copyright = "2021, Artur Lissin"
+author = "Artur Lissin"
+release = "v0.1.0"
 
 
 # -- General configuration -----------------------------------------------------
@@ -27,12 +25,12 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
     "sphinx.ext.ifconfig",
-    "sphinx_autodoc_typehints"
+    "sphinx_autodoc_typehints",
 ]
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-source_suffix = '.rst'
-pygments_style = 'sphinx'
+source_suffix = ".rst"
+pygments_style = "sphinx"
 
 
 # -- Options for numpydoc ------------------------------------------------------
@@ -49,10 +47,13 @@ numpydoc_xref_aliases = {
     "TaxCalculator": "src.cashier.purchase.tax_calculator.TaxCalculator",
     "Callable": "collections.abc.Callable",
     "Path": "pathlib.Path",
-    "Decimal": "decimal.Decimal"
+    "Decimal": "decimal.Decimal",
 }
 numpydoc_xref_ignore = {
-    'optional', 'type_without_description', 'BadException', 'default'
+    "optional",
+    "type_without_description",
+    "BadException",
+    "default",
 }
 numpydoc_validation_checks = {"all", "GL01", "SA04", "RT03"}
 numpydoc_validation_excludeset = {}
@@ -60,7 +61,7 @@ numpydoc_validation_excludeset = {}
 
 # -- Options for intersphinx ---------------------------------------------------
 
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 
 
 # -- Options for autosummary ---------------------------------------------------
@@ -82,19 +83,19 @@ autodoc_inherit_docstrings = False
 
 html_theme = "pydata_sphinx_theme"
 html_theme_options = {
-  "icon_links": [
-      {
-          "name": "GitHub",
-          "url": "https://github.com/arturOnRails/Cashier",
-          "icon": "fab fa-github-square",
-      }
-  ]
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/arturOnRails/Cashier",
+            "icon": "fab fa-github-square",
+        }
+    ]
 }
 html_title = f"Cashier {get_version()} documentation"
-html_static_path = ['_static']
+html_static_path = ["_static"]
 master_doc = "index"
 html_use_modindex = True
 html_copy_source = False
 html_domain_indices = True
-html_file_suffix = '.html'
+html_file_suffix = ".html"
 add_module_names = False
